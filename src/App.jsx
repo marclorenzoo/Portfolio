525a396e0fec2606e35b7fb031b31f46403746d4
mlorenzoo <malool@fp.insjoaquimmir.cat>
2024-10-03 18:44:07 +0200

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import './App.css'
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
-  return <>Hello World</>;
+  return <div className={styles.App}>
+    <Navbar />
+  </div>;
 }
 
 export default App
